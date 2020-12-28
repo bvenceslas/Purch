e65f0a7d03189de900f0b07235d00f45d64c0fee
bvenceslas <bvenceslas@gmail.com>
2020-12-28 10:33:06 +0200

    create purch stored procedures

--- src/brain/db/script_purch.sql
+++ src/brain/db/script_purch.sql
@@ -12,7 +12,7 @@ go
 create table t_access_setup
 (
 	id int not null,	
-	access_setup varchar(255) not null, -- super admin, admin, vendeur, magasinier
+	access_level varchar(255) not null, -- super admin, admin, vendeur, magasinier
 	primary key clustered
 	(
 		id
@@ -88,7 +88,6 @@ create table t_product
 (
 	id int not null,
 	product varchar(255),
-	category varchar(255),
 	quantification text, 
 	price float,
 	stock float default 0,
@@ -231,3 +230,267 @@ alter table t_payment with check add constraint fk_purchase_payment foreign key(
 references t_purchase(id)
 alter table t_payment with check add constraint fk_user_payment foreign key(username) 
 references t_login(username)
+
+
+-- Creation of Stored procedures
+
+--access_setup
+go
+create procedure sp_update_access_setup
+(	
+	@id int,
+	@access_level varchar(255)
+)
+as 
+begin
+	if not exists(select * from t_access_setup where id = @id)
+		insert into t_access_setup values(@id, @access_level)
+	else
+		update t_access_setup set access_level = @access_level where id = @id
+end
+
+--login
+go
+create procedure sp_update_login
+(
+	@username varchar(255),
+	@pwd varchar(255),
+	@access_level varchar(50)
+)
+as
+begin
+	if not exists(select * from t_login where username = @username)
+		insert into t_login values (@username, @pwd, @access_level)
+	else
+		update t_login set pwd = @pwd, access_level = @access_level where username = @username
+end
+
+
+--soft
+go 
+create procedure sp_update_soft
+(
+	@id int,
+	@soft varchar(255)
+)
+as
+begin
+	if not exists(select * from t_soft where id = @id)
+		insert into t_soft values(@id, @soft)
+	else
+		update t_soft set soft = @soft where id = @id
+end
+
+--gamme
+go 
+create procedure sp_update_gamme
+(
+	@id int,
+	@gamme varchar(255)
+)
+as
+begin
+	if not exists(select * from t_gamme where id = @id)
+		insert into t_gamme values(@id, @gamme)
+	else
+		update t_gamme set gamme = @gamme where id = @id
+end
+
+--category
+go 
+create procedure sp_update_tcategory
+(
+	@id int,
+	@category varchar(255),
+	@gamme int 
+)
+as
+begin
+	declare @id_gamme int = (select id from t_gamme where gamme = @gamme)
+	if not exists(select * from t_category where id = @id)
+		insert into t_category values(@id, @category, @id_gamme)
+	else
+		update t_category set category = @category, id_gamme = @id_gamme where id = @id
+end
+
+
+--quantification
+go 
+create procedure sp_update_quantification
+(
+	@id int,
+	@quantification varchar(255)
+)
+as
+begin
+	if not exists(select * from t_quantification where id = @id)
+		insert into t_quantification values(@id, @quantification)
+	else
+		update t_quantification set quantification = @quantification where id = @id
+end
+
+
+--product
+go
+create procedure sp_update_product
+(
+	@id int,
+	@product varchar(255),
+	@quantification text, 
+	@price float,
+	@stock float,
+	@categoy varchar(255)
+)
+as
+begin
+	declare @id_category int = (select id from t_category where category = @categoy)
+	if not exists(select * from t_product where id = @id)
+		insert into t_product values(@id, @product, @quantification, @price, @stock, @id_category)
+	else
+		update t_product set product = @product, quantification = @quantification, price = @price, stock = @stock, id_categoy = @id_category where id = @id
+end
+
+--provider
+go
+create procedure sp_update_provider
+(
+	@id int,
+	@nom varchar(50), 
+	@prenom varchar(50), 
+	@contact varchar(13), 
+	@addresse varchar(255)
+)
+as
+begin
+	if not exists(select * from t_provider where id = @id)
+		insert into t_provider values(@id, @nom, @prenom, @contact, @addresse)
+	else
+		update t_provider set nom = @nom, prenom = @prenom, contact = @contact, addresse = @addresse where id = @id
+end
+
+
+--client
+go
+create procedure sp_update_client
+(
+	@id int,
+	@nom varchar(50), 
+	@prenom varchar(50), 
+	@contact varchar(13), 
+	@addresse varchar(255)
+)
+as
+begin
+	if not exists(select * from t_client where id = @id)
+		insert into t_client values(@id, @nom, @prenom, @contact, @addresse)
+	else
+		update t_client set nom = @nom, prenom = @prenom, contact = @contact, addresse = @addresse where id = @id
+end
+
+
+--line providing and providding
+go
+create procedure sp_update_line_providding 
+(
+	@id int,
+	@provider varchar(255),
+	@date_providding date,
+	@id_providding int,
+	@product  varchar(255),
+	@qty float,
+	@price float
+)
+as
+begin
+	-- get provider id
+	declare @id_provider int = (select id  from t_provider where concat(nom, ' ', prenom) = @provider)
+	-- get product id
+	declare @id_product int = (select id  from t_product where product = @product)
+
+	-- first the providding table
+	if not exists(select * from t_providding where id = @id)
+		insert into t_providding values (@id, @id_provider, @date_providding)
+	else
+		update t_providding set id_provider = @id_provider, date_providding = @date_providding where id = @id
+
+	-- now the line_providding
+	if not exists (select * from t_line_providding where id_providding = @id and id_product = @id_product)
+		begin
+			insert into t_line_providding values(@id, @id_product, @qty, @price)
+			update t_product set stock = (stock + @qty) where id = @id_product
+		end
+	else
+		begin
+			update t_line_providding set qty = @qty, price = @price where id_providding = @id and id_product = @id_product
+			-- decrease the last value of the quantity
+			declare @qty_init float = (select qty from t_line_providding where id_providding = @id and id_product = @id_product)
+			update t_product set stock = (stock - @qty_init) + @qty where id = @id_product
+		end
+end
+
+
+-- line purchase and purchase
+go
+create procedure sp_update_line_purchase
+(
+	@id int,
+	@client varchar(255),
+	@username varchar(255),
+	@date_purchase date,
+	@id_purchase int,
+	@product varchar(255),
+	@qty float,
+	@reduction float
+)
+as
+begin
+	-- get client id
+	declare @id_client int = (select id  from t_client where concat(nom, ' ', prenom) = @client)
+	-- get product id
+	declare @id_product int = (select id  from t_product where product = @product)
+	-- get the stock of the product
+	declare @actual_stock float = (select stock from t_product where id = @id_product)
+		if @qty <= @actual_stock
+			begin
+				-- first the purchase table
+				if not exists(select * from t_purchase where id = @id)
+					insert into t_purchase values (@id, @id_client, @date_purchase, @username)
+				else
+					update t_purchase set id_client = @id_client, date_purchase = @date_purchase, username = @username where id = @id
+
+				-- now the line_purchase
+				if not exists (select * from t_line_purchase where id_purchase = @id and id_product = @id_product)
+					begin
+						insert into t_line_purchase values(@id, @id_product, @qty, @reduction)
+						update t_product set stock = (stock - @qty) where id = @id_product
+					end
+				else
+					begin
+						update t_line_purchase set qty = @qty, reduction = @reduction where id_purchase = @id and id_product = @id_product
+						-- increase the value of the quantity
+						declare @qty_init float = (select qty from t_line_purchase where id_purchase = @id and id_product = @id_product)
+						update t_product set stock = (stock + @qty_init) - @qty where id = @id_product
+					end
+			end
+		else
+			print 'STOCK INSUFFISANT'
+end
+
+
+--payment
+go
+create procedure sp_update_payment
+(
+	@id int,
+	@id_purchase int,
+	@amount float,
+	@date_payment date,
+	@username varchar(255)
+)
+as
+begin
+	if not exists(select * from t_payment where id = @id)
+		insert into t_payment values(@id, @id_purchase, @amount, @date_payment, @username)
+	else
+		update t_payment set id_purchase = @id_purchase, amount = @amount, date_payment = @date_payment, username = @username where id = @id
+end
